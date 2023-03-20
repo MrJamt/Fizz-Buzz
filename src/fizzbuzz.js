52991adaeb8e168cded0e0da63efb74349005f42
Adrian Montano <adrian.montano2011@yahoo.com>
2023-03-20 12:58:42 -0400

    Se implementó la primera prueba generar fizzbuzz para un solo numero, se creo la función fizzbuzz para que pase y finalmente se refactorizó el codigo de producción en un archivo fizzbuzz.js

---
+++ src/fizzbuzz.js
@@ -0,0 +1,7 @@
+//CODIGO DE PRODUCCION DEL METODO
+function fizzbuzz(num)
+{
+    return '1';
+}
+
+export default fizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,6 +1,8 @@
+//PRUEBAS / ESPECIFICACION
+import fizzbuzz from "./fizzbuzz.js";
 
 describe("FizzBuzz", () => {
-  it("", () => {
-    //expect(true).toEqual(true);
+  it("Generar FizzBuzz para un solo numero", () => {
+    expect(fizzbuzz(1)).toEqual('1');
   });
 });
